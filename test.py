4595fc8cc4d427e6972c00584546e54e80edf8a5
Svetlana <lampa1979@mail.ru>
2021-06-01 10:11:03 +0400

    Change test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+age = 18
+name = 'Ivan'
+print('My name is: ', name, 'i*m' , age, 'years old')
+
+age1 = input('Введите ваш возраст')
+name1 = input('Введите ваше имя')
+print('Мое имя: ', name1, 'Мой возраст' , age1)
