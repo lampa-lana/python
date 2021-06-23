e66396d4b2ed279bab93713df220c22f0b2410a4
Svetlana <lampa1979@mail.ru>
2021-06-23 15:23:09 +0400

    create homework06

--- test.py
+++ test.py
@@ -1,179 +1,100 @@
-import math
-import random
-import copy
-import re
 import os
+import sys
+# ------------------------------------Easy-------------------------------------------
 # EASY
-# Все задачи текущего блока решите с помощью генераторов списков!
-
-# Задание-1:
-# Дан список, заполненный произвольными целыми числами.
-# Получить новый список, элементы которого будут
-# квадратами элементов исходного списка
-# [1, 2, 4, 0] --> [1, 4, 16, 0]
-
-my_list = [1, 2, 4, 0]
-print(my_list)
-#new_list = list(map(lambda x: x**2, my_list.copy()))
-new_list = [i ** 2 for i in my_list.copy()]
-print(my_list, new_list)
+# Задача-1:
+# Следующая программа написана верно, однако содержит места потенциальных ошибок.
+# используя конструкцию try добавьте в код обработку соответствующих исключений.
+# Пример.
+# Исходная программа:
 
 
-# Задание-2:
-# Даны два списка фруктов.
-# Получить список фруктов, присутствующих в обоих исходных списках.
+def avg(a, b):
+    """Вернуть среднее геометрическое чисел 'a' и 'b'.
 
-# lst1 = ['банан', 'апельсин',   'мандарин', 'малина', 'яблоко',  'киви']
-# lst2 = ['банан', 'апельсин',   'груша',  'черешня',  'клубника',  'малина']
-# lst3 = []
+    Параметры:
+        - a, b (int или float).
 
-
-lst1 = ['банан', 'апельсин',   'мандарин', 'малина', 'яблоко',  'киви']
-lst2 = ['банан', 'апельсин',   'груша',  'черешня',  'клубника',  'малина']
-
-
-def fruits():
-    lst3 = []
-    for i in lst1.copy():
-        for j in lst2.copy():
-            if i == j:
-                lst3.append(i)
-    print(lst3)
+    Результат:
+        - float.
+    """
+    if a * b >= 0:
+        return (a * b) ** 0.5
+    else:
+        raise ValueError(
+            " Невозможно определить среднее геометрическое, введеных Вами чисел ")
 
 
-fruits()
-
-lst4 = [i for i in lst1.copy() if i in lst2.copy()]
-print(lst4)
-
-# Задание-3:
-# Дан список, заполненный произвольными числами.
-# Получить список из элементов исходного, удовлетворяющих следующим условиям:
-# + Элемент кратен 3
-# + Элемент положительный
-# + Элемент не кратен 4
-
-super_list = [1, 2, 5, 7, 9, 10, 12, 25, -100, 120, -3, -6, 0]
-super_list2 = []
-for i in super_list:
-    if i > 0 and i % 3 == 0 and i % 4 != 0:
-        super_list2.append(i)
-print(super_list2)
-
-super_list3 = [i for i in super_list if i > 0 and i % 3 == 0 and i % 4 != 0]
-print(super_list3)
-
-# NORMAL
-# Задание-1:
-# Вывести символы в нижнем регистре, которые находятся вокруг
-# 1 или более символов в верхнем регистре.
-# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
-# Решить задачу двумя способами: с помощью re и без.
-
-line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
-       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
-       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
-       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
-       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
-       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
-       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
-       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
-       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
-       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
-       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
-       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
-       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
-       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
-       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
-line_re = re.findall(r'[a-z]+', line)
-print('Line_re: \n', line_re)
+# a = float(input("a = "))
+# b = float(input("b = "))
+try:
+    a = float(input("a = "))
+    b = float(input("b = "))
+    c = avg(a, b)
+    print("Среднее геометрическое = {:.2f}".format(c))
+except ValueError as err:
+    print("Ошибка! Вы ввели значения несовместимых типов: " + str(err))
+except Exception as ve:
+    print("Ошибка: " + str(ve))
+finally:
+    print('thanks for your cooperation')
 
 
-# Задание-2:
-# Вывести символы в верхнем регистре, слева от которых находятся
-# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки
-# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
-# нужно получить список строк: ['AY', 'NOGI', 'P']
-# Решить задачу двумя способами: с помощью re и без.
+# ПРИМЕЧАНИЕ: Для решения задач 2-4 необходимо познакомиться с модулями os, sys!
+# СМ.: https://pythonworld.ru/moduli/modul-os.html, https://pythonworld.ru/moduli/modul-sys.html
 
-line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
-    'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
-    'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
-    'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
-    'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
-    'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
-    'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
-    'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
-    'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
-    'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
-    'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
-    'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
-    'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
-    'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
-    'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+# Задача-2:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
 
-line_re2 = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
-print('Line_re2: \n', line_re2)
+# Задача-3:
+# Напишите скрипт, отображающий папки текущей директории.
+
+# Задача-4:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
 
-# Задание-3:
-# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
-# произвольными целыми цифрами, в результате в файле должно быть
-# 2500-значное произвольное число.
-# Найдите и выведите самую длинную последовательность одинаковых цифр
-# в вышезаполненном файле.
+# ---------------------------------------Normal-------------------------------------
+# Normal
+# Задача-1:
+# Примечание: Если уже делали easy задание, то просто перенесите решение сюда.
+# Следующая программа написана верно, однако содержит места потенциальных ошибок.
+# используя конструкцию try добавьте в код обработку соответствующих исключений.
+# Пример.
+# Исходная программа:
+# def avg(a, b):
+#     """Вернуть среднее геометрическое чисел 'a' и 'b'.
 
-# HARD
-# Задание-1:
-# Матрицы в питоне реализуются в виде вложенных списков:
-# Пример. Дано:
-matrix = [[1, 0, 8],
-          [3, 4, 1],
-          [0, 4, 2]]
-print(matrix)
+#     Параметры:
+#         - a, b (int или float).
+
+#     Результат:
+#         - float.
+#     """
+#     return (a * b) ** 0.5
 
 
-# Выполнить поворот (транспонирование) матрицы
-# Пример. Результат:
-# matrix_rotate = [[1, 3, 0],
-#                  [0, 4, 4],
-#                  [8, 1, 2]]
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = avg(a, b)
+# print("Среднее геометрическое = {:.2f}".format(c))
 
-print('rotate_matrix = ', list(map(list, zip(*matrix))))
+# ПРИМЕЧАНИЕ: Для решения задачи 2-3 необходимо познакомиться с модулями os, sys!
+# СМ.: https://pythonworld.ru/moduli/modul-os.html, https://pythonworld.ru/moduli/modul-sys.html
 
-# Суть сложности hard: Решите задачу в одну строку
+# Задача-2:
+# Напишите небольшую консольную утилиту,
+# позволяющую работать с папками текущей директории.
+# Утилита должна иметь "меню" выбора действия, в котором будут пункты:
+# 1. Перейти в папку
+# 2. Просмотреть содержимое текущей папки
+# 3. Удалить папку
+# 4. Создать папку
+# При выборе пунктов 1, 3, 4 программа запрашивает название папки
+# и выводит результат действия: "Успешно создано/удалено/перешел",
+# "Невозможно создать/удалить/перейти"
 
-# Задание-2:
-# Найдите наибольшее произведение пяти последовательных цифр в 1000-значном числе.
-# Выведите произведение и индекс смещения первого числа последовательных 5-ти цифр.
-# Пример 1000-значного числа:
-number = """
-73167176531330624919225119674426574742355349194934
-96983520312774506326239578318016984801869478851843
-85861560789112949495459501737958331952853208805511
-12540698747158523863050715693290963295227443043557
-66896648950445244523161731856403098711121722383113
-62229893423380308135336276614282806444486645238749
-30358907296290491560440772390713810515859307960866
-70172427121883998797908792274921901699720888093776
-65727333001053367881220235421809751254540594752243
-52584907711670556013604839586446706324415722155397
-53697817977846174064955149290862569321978468622482
-83972241375657056057490261407972968652414535100474
-82166370484403199890008895243450658541227588666881
-16427171479924442928230863465674813919123162824586
-17866458359124566529476545682848912883142607690042
-24219022671055626321111109370544217506941658960408
-07198403850962455444362981230987879927244284909188
-84580156166097919133875499200524063689912560717606
-05886116467109405077541002256983155200055935729725
-71636269561882670428252483600823257530420752963450"""
-
-# Задание-3 (Ферзи):
-# Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били
-# друг друга. Вам дана расстановка 8 ферзей на доске.
-# Определите, есть ли среди них пара бьющих друг друга.
-# Программа получает на вход восемь пар чисел,
-# каждое число от 1 до 8 — координаты 8 ферзей.
-# Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
+# Для решения данной задачи используйте алгоритмы из задания easy,
+# оформленные в виде соответствующих функций,
+# и импортированные в данный файл из easy.py
