ba2aa0660adb5d89db6b95501cbcd76b126b1edc
Svetlana <lampa1979@mail.ru>
2021-07-15 16:27:43 +0400

    create homework12

---
+++ server.py
@@ -0,0 +1,30 @@
+import socket
+import time
+
+host = 'localhost'
+port = 9090
+clients = []
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+quit = False
+print('[Server Srartet]')
+
+while not quit:
+    try:
+        data, addr = s.recvfrom(1024)
+        if addr not in clients:
+            clients.append(addr)
+            itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
+            print('[' + addr[0] + '] = [' + str(addr[1]) +
+                  '] = [' + itsatime + '] /', end='')
+            print(data.decode('utf-8'))
+            for client in clients:
+                if addr != client:
+                    s.sendto(data, client)
+    except Exception as ex:
+        print(ex)
+        print('\n[ Server Stoppped]')
+        quit = True
+s.close()
