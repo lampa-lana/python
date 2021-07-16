a389f8330aee9d9892a8202fa1d3756df621cd02
Svetlana <lampa1979@mail.ru>
2021-07-16 15:03:07 +0400

    Change homework12

--- server.py
+++ server.py
@@ -16,13 +16,13 @@ while not quit:
         data, addr = s.recvfrom(1024)
         if addr not in clients:
             clients.append(addr)
-            itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
-            print('[' + addr[0] + '] = [' + str(addr[1]) +
-                  '] = [' + itsatime + '] /', end='')
-            print(data.decode('utf-8'))
-            for client in clients:
-                if addr != client:
-                    s.sendto(data, client)
+        itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
+        print('[' + addr[0] + '] = [' + str(addr[1]) +
+              '] = [' + itsatime + '] /', end='')
+        print(data.decode('utf-8'))
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
     except Exception as ex:
         print(ex)
         print('\n[ Server Stoppped]')
