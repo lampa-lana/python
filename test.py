0b7c2312af245882210f556717cd9356f04264e4
Svetlana <lampa1979@mail.ru>
2021-07-29 16:28:12 +0400

    Change homework09

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ class Tag(object):
     html = ''  # тег
     attrs = ''  # атрибуты
     decr = ''  # описание
+    tag = ''
 
     def get_html(self):
         return self.html
@@ -15,34 +16,37 @@ class Tag(object):
     def get_decr(self):
         return self.decr
 
+    def get_tag(self):
+        return Tag.get_html(self) + ' ' + Tag.get_attrs(self) + ' ' + Tag.get_decr(self)
+
 
 class Input(Tag):
     html = '<input>'
-    attrs = 'alt, checked'
+    attrs = ' alt, checked -  атрибуты тега. '
     decr = 'Позволяет создавать разные части интерфейса и обеспечивать взаимодействие с пользователем. Закрывающий тег не требуется'
 
 
 class Image(Tag):
     html = '<img>'
-    attrs = 'src, alt'
+    attrs = ' src, alt - атрибуты тега. '
     decr = 'Предназначен для отображения на веб-странице изображений в графическом формате.Закрывающий тег не требуется'
 
 
 class P(Tag):
     html = '<p></p>'
-    attrs = 'align - атрибут устарел'
+    attrs = 'align - атрибуты тега. '
     decr = 'Определяет текстовый абзац.Закрывающий тег не требуется'
 
 
 class A(Tag):
     html = '<a></a>'
-    attrs = 'href'
+    attrs = ' href - атрибуты тега. '
     decr = 'Предназначен для создания ссылок'
 
 
 class Form(Tag):
     html = '<form></form>'
-    attrs = 'action'
+    attrs = ' action - атрибуты тега. '
     decr = 'Устанавливает форму на веб-странице'
 
 
@@ -57,3 +61,8 @@ tags = ['image', 'input', 'p', 'a', 'form']
 for i in tags:
     print(f'Тег <{i}> пишется: ', factory.create_tag(i).get_html(), factory.create_tag(i).get_decr(),
           ' имеет атрибуты:', factory.create_tag(i).get_attrs())
+print('-----------------------------------------------------------------------------------------------------')
+
+for i in tags:
+    print('Тег <{}> пишется: '.format(
+        i), factory.create_tag(i).get_tag())
